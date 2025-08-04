11778841513751d2933f564c21f8c83e4bb493a5
thomas <thomasjcabovianco@gmail.com>
2025-08-04 18:01:06 -0300

    Version 2.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.cabovianco.remindme"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
+        versionCode = 8
         versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
